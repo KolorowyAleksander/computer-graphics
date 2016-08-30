b4d1a4d848dfa2cf43d6f5d30c29e5f4e4d31f9f
atka <pomaranczowa3>
2016-08-31 00:44:27 +0200

    settings added 2.0

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <iostream>
 #include "Camera.h"
+#include "Settings.h"
 
 
 Camera *Camera::instance(nullptr);
--- src/Settings.h
+++ src/Settings.h
@@ -4,10 +4,19 @@
 
 #ifndef GRAFIKA_SETTINGS_H
 #define GRAFIKA_SETTINGS_H
-
+#include <yaml-cpp/yaml.h>
 
 class Settings {
 
+ private:
+    static Settings *instance;
+    YAML::Node config;
+    Settings();
+
+ public:
+    static Settings *getInstance();
+    int getWindowHeight();
+    int getWindowWidth();
 };
 
 
