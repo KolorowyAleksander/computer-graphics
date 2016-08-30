d678679e7dba636ae3d384d4cbd0aac1f2fd2615
atka <pomaranczowa3>
2016-08-31 00:35:51 +0200

    added settings

---
+++ src/Settings.cpp
@@ -0,0 +1,5 @@
+//
+// Created by atka on 30.08.16.
+//
+
+#include "Settings.h"
---
+++ src/Settings.h
@@ -0,0 +1,14 @@
+//
+// Created by atka on 30.08.16.
+//
+
+#ifndef GRAFIKA_SETTINGS_H
+#define GRAFIKA_SETTINGS_H
+
+
+class Settings {
+
+};
+
+
+#endif //GRAFIKA_SETTINGS_H
